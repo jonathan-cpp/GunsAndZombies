8eaa28f33ea1116028eceacdc8554afd034e8c7f
Jonathan <jonathanhelsing84@gmail.com>
2024-02-03 19:21:42 +0100

    Added documentation to INetwork

--- Common/Source/Interfaces/INetwork.h
+++ Common/Source/Interfaces/INetwork.h
@@ -50,42 +50,78 @@ public:
     // Update the network logic with the given time interval.
     virtual void Update(float deltaTime) = 0;
 
-    // Function Callbacks
+    // Event triggered when a peer has been validated after connecting.
+    // Parameters:
+    //   - sf::TcpSocket*: The TCP socket of the validated peer.
+    //   - sf::IpAddress: The IP address of the validated peer.
+    //   - unsigned short: The port number of the validated peer.
+    DECLARE_EVENT(PeerValidatedEvent, sf::TcpSocket*, sf::IpAddress, unsigned short);
+    PeerValidatedEvent OnPeerValidated;
+
+protected:
+    // Event triggered when a new peer successfully connects.
+    // Parameters:
+    //   - sf::TcpSocket*: The TCP socket of the newly connected peer.
     DECLARE_EVENT(PeerConnectedEvent, sf::TcpSocket*);
     PeerConnectedEvent OnPeerConnected;
 
+    // Event triggered when a peer disconnects.
+    // Parameters:
+    //   - sf::TcpSocket*: The TCP socket of the disconnected peer.
     DECLARE_EVENT(PeerDisconnectedEvent, sf::TcpSocket*);
     PeerDisconnectedEvent OnPeerDisconnected;
 
+    // Event triggered when a TCP packet is received from a peer.
+    // Parameters:
+    //   - sf::Packet&: The received TCP packet.
     DECLARE_EVENT(TcpPacketReceivedEvent, sf::Packet&);
     TcpPacketReceivedEvent OnTcpPacketReveived;
 
+    // Event triggered when a UDP packet is received from a peer.
+    // Parameters:
+    //   - sf::Packet&: The received UDP packet.
+    //   - sf::IpAddress: The IP address of the sender.
+    //   - unsigned short: The port number of the sender.
     DECLARE_EVENT(UdpPacketReceivedEvent, sf::Packet&, sf::IpAddress, unsigned short);
     UdpPacketReceivedEvent OnUdpPacketReveived;
 
-protected:
-    // This function checks the status of the TCP listener socket and
-    // accept any incomming connection.
+    // Function checks the status of the TCP listener
+    // and accepts any incoming connection.
     void HandleIncomingConnection();
 
     // Function to handle the reception of TCP packets.
-    // It evokes ProcessTcpPacket upon packet received.
+    // Parameters:
+    //   - socket: Pointer to the TCP socket for which the packet is received.
     void ReceiveTcpPacket(sf::TcpSocket* socket);
 
     // Function to handle the reception of UDP packets.
-    // It evokes ProcessUdpPacket upon packet received.
     void ReceiveUdpPacket();
 
     // Send a TCP packet to the specified TCP socket.
+    // Parameters:
+    //   - packet: Reference to the SFML packet to be sent.
+    //   - socket: Pointer to the TCP socket to which the packet is sent.
     void SendTcpPacket(sf::Packet &packet, sf::TcpSocket* socket);
 
     // Send a UDP packet to the specified UDP socket.
+    // Parameters:
+    //   - packet: Reference to the SFML packet to be sent.
+    //   - remoteAddress: IP address of the remote host to which the packet is sent.
+    //   - remotePort: Port number of the remote host to which the packet is sent.
     void SendUdpPacket(sf::Packet &packet, sf::IpAddress remoteAddress, unsigned short remotePort);
 
-    // Serialize higher-level data into a SFML Packet for transmission.
+    // Serialize higher-level data into an SFML Packet for transmission.
+    // Parameters:
+    //   - const std::any& data: The data to be serialized.
+    // Returns:
+    //   - sf::Packet: The serialized data in an SFML Packet.
     virtual sf::Packet SerializeData(const std::any& data) const { return sf::Packet(); }
 
     // Deserialize an SFML Packet into higher-level data.
+    // Parameters:
+    //   - const sf::Packet& packet: The SFML Packet to be deserialized.
+    // Returns:
+    //   - std::any: The deserialized data.
     virtual std::any DeserializeData(const sf::Packet& packet) const { return std::any(); }
 
 private:
